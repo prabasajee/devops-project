3b107e6a6607b28335a3231df54fa75380e08797
praba <prabaharansajeevan23@gamil.com>
2025-06-10 16:21:29 +0530

    "new change to app.py"

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, DevOps World!"
+
+@app.route('/time')
+def get_time():
+    return f"Current server time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
